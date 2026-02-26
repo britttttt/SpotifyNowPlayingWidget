db77a5e7c1328278bd6b223181776cc3d95e1510
Brit Beaty <britneybeaty@gmail.com>
2026-02-26 16:17:47 -0600

    Connected JS script to server logic, added boilerplate HTML, and Added basic styling

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
 async function poll() {
   const res  = await fetch('/now-playing')
   const data = await res.json()
-  console.log(data)  
+  console.log(data)
+
+  if (data && data.title) {
+    document.getElementById('title').textContent  = data.title
+    document.getElementById('artist').textContent = data.artist
+    document.getElementById('art').src = data.album_art
+  }
 }
 
 poll()
 setInterval(poll, 3000)
-
-document.getElementById('title').textContent = data.title
-document.getElementById('artist').textContent = data.artist
-document.getElementById('art').src = data.album_art
